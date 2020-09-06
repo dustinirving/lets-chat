37e89b715189252d2e475c2bba1037828e7856c2
Dustin Irving <dustin.irving@gmail.com>
2020-09-06 09:37:58 -0400

    Proxy default

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { WebSocketLink } from '@apollo/client/link/ws'
 import { SubscriptionClient } from 'subscriptions-transport-ws'
 
-const frontend = new SubscriptionClient('/graphql', {
+const frontend = new SubscriptionClient('ws://localhost:4000/graphql', {
   reconnect: true
 })
 
